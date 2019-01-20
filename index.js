07c29c8899548b34970b50a744cfba5f12918d34
Jerome Fitzgerald <jeromes@gmail.com>
2019-01-20 12:26:04 -0500

    ✨️ +type (final_svg), return {}
    This is hyper-custom, but if you do not
     pass anything it will default to `final_svg`.
    
    If you pass `svg_base64encoded` it will
     output a `.txt` file with the data encode.
    
    Probably a better way to get this info, or report it.
    
    🤷️

--- index.js
+++ index.js
@@ -3,73 +3,69 @@
 const PLUGIN_NAME = "gulp-sqippy";
 
 const sqip = require("sqip"),
-	through = require("through2"),
-	path = require("path"),
-	fs = require("fs"),
-	log = require("fancy-log");
+  through = require("through2"),
+  path = require("path"),
+  fs = require("fs"),
+  log = require("fancy-log");
 
 const sqipModes = [
-	{
-		name: "combo",
-		code: 0,
-	},
-	{
-		name: "triangle",
-		code: 1,
-	},
-	{
-		name: "rect",
-		code: 2,
-	},
-	{
-		name: "ellipse",
-		code: 3,
-	},
-	{
-		name: "circle",
-		code: 4,
-	},
-	{
-		name: "rotatedrect",
-		code: 5,
-	},
-	{
-		name: "beziers",
-		code: 6,
-	},
-	{
-		name: "rotatedellipse",
-		code: 7,
-	},
-	{
-		name: "polygon",
-		code: 8,
-	},
+  {
+    name: "combo",
+    code: 0
+  },
+  {
+    name: "triangle",
+    code: 1
+  },
+  {
+    name: "rect",
+    code: 2
+  },
+  {
+    name: "ellipse",
+    code: 3
+  },
+  {
+    name: "circle",
+    code: 4
+  },
+  {
+    name: "rotatedrect",
+    code: 5
+  },
+  {
+    name: "beziers",
+    code: 6
+  },
+  {
+    name: "rotatedellipse",
+    code: 7
+  },
+  {
+    name: "polygon",
+    code: 8
+  }
 ];
 
 /**
  * run SQIP to generate the SVG code
  * @param filepath - The source file path
  * @param options - primitives, blur and mode
- * @returns {string} - The SVG XML code
+ * @returns {object} - { final_svg, img_dimensions, svg_base64encoded}
  */
-var makeSvgPlaceholder = function(filepath, options) {
-	if (typeof options == "undefined") {
-		var options = {};
-	}
-
-	const resultObject = sqip({
-		filename: filepath,
-		numberOfPrimitives: parseInt(options.primitives) || 20,
-		blur: options.blur || 10,
-		mode: options.mode || 5,
-	});
-	return resultObject.final_svg;
+var makePlaceholder = function(filepath, options) {
+  const resultObject = sqip({
+    filename: filepath,
+    numberOfPrimitives: parseInt(options.primitives),
+    blur: options.blur,
+    mode: options.mode
+  });
+  return resultObject;
 };
 
 var getFileSizeMb = function(filePath) {
-	const stats = fs.statSync(filePath);
-	return stats.size / 1000000.0;
+  const stats = fs.statSync(filePath);
+  return stats.size / 1000000.0;
 };
 
 /**
@@ -77,56 +73,73 @@ var getFileSizeMb = function(filePath) {
  * @param options - primitives, blur and mode
  */
 var gulpSqip = function(options) {
-	if (typeof options == "undefined") {
-		var options = {};
-		options.includeSource = options.includeSource === false ? false : true;
-	}
+  if (typeof options == "undefined") {
+    // defaults
+    var options = {
+      blur: 10,
+      mode: 5,
+      primitives: 20,
+      type: "final_svg" // final_svg, img_dimensions, svg_base64encoded
+    };
+    options.includeSource = options.includeSource === false ? false : true;
+  }
 
-	return through.obj(function(sourceFile, enc, callback) {
-		const sourceInfo = path.parse(sourceFile.path);
-		if (sourceFile && sourceFile.isBuffer()) {
-			// log which file is being processed
-			let sizeText;
-			try {
-				sizeText = getFileSizeMb(sourceFile.path).toFixed(1) + "mb";
-			} catch (err) {
-				sizeText = "unknown";
-			}
-			log(
-				"Processing '%s' (%s with %s primitives)",
-				sourceInfo.name + sourceInfo.ext,
-				sizeText,
-				options.primitives || "default"
-			);
-			// make the new file
-			const sqipResult = makeSvgPlaceholder(sourceFile.path, options);
-			const svgFile = sourceFile.clone();
-			svgFile.contents = Buffer.from(sqipResult);
-			// set new file name
-			const appendName = typeof options.appendName == "string" ? options.appendName : "";
-			const prependName =
-				typeof options.prependName == "string" ? options.prependName : "";
-			svgFile.path = path.join(
-				sourceInfo.dir,
-				prependName + sourceInfo.name + appendName + ".svg"
-			);
-			// send file(s) along
-			this.push(svgFile);
-			if (options.includeSource) {
-				this.push(sourceFile);
-			}
-			callback();
-		} else {
-			this.emit(
-				"error",
-				new PluginError(
-					PLUGIN_NAME,
-					"Source must be a file in buffer format: " + sourceFile.path
-				)
-			);
-			callback();
-		}
-	});
+  return through.obj(function(sourceFile, enc, cb) {
+    const sourceInfo = path.parse(sourceFile.path);
+    if (sourceFile && sourceFile.isBuffer()) {
+      // log which file is being processed
+      let sizeText;
+      try {
+        sizeText = getFileSizeMb(sourceFile.path).toFixed(1) + "mb";
+      } catch (err) {
+        sizeText = "unknown";
+      }
+      log(
+        "Processing '%s' (%s with %s primitives)",
+        sourceInfo.name + sourceInfo.ext,
+        sizeText,
+        options.primitives || "default"
+      );
+      // make the new file
+      const sqipResult = makePlaceholder(sourceFile.path, options)[
+        options.type
+      ];
+      const svgFile = sourceFile.clone();
+      const appendText =
+        options.type !== "final_svg" ? "data:image/svg+xml;base64," : "";
+      svgFile.contents = Buffer.from(`${appendText}${sqipResult}`);
+
+      // set new file name
+      const appendName =
+        typeof options.appendName == "string" ? options.appendName : "";
+      const prependName =
+        typeof options.prependName == "string" ? options.prependName : "";
+
+      svgFile.path = path.join(
+        sourceInfo.dir,
+        prependName +
+          sourceInfo.name +
+          appendName +
+          (options.type !== "final_svg" ? ".txt" : ".svg")
+      );
+
+      // send file(s) along
+      this.push(svgFile);
+      if (options.includeSource) {
+        this.push(sourceFile);
+      }
+      cb();
+    } else {
+      this.emit(
+        "error",
+        new PluginError(
+          PLUGIN_NAME,
+          "Source must be a file in buffer format: " + sourceFile.path
+        )
+      );
+      cb();
+    }
+  });
 };
 
 module.exports = gulpSqip;
