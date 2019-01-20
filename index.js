417fa4596b30cd595453746ffde0c3f477edf5d6
Jerome Fitzgerald <jeromes@gmail.com>
2019-01-20 12:30:42 -0500

    🎨️ s-c-o-t-t .editorconfig 📝️ md format
    This reverts the code back to the tab 4 space.
    
    If we want to move to `prettier`,
     I wouldn't object. 😀️
    
    Validates the MD formatting as well.

--- index.js
+++ index.js
@@ -3,48 +3,48 @@
 const PLUGIN_NAME = "gulp-sqippy";
 
 const sqip = require("sqip"),
-  through = require("through2"),
-  path = require("path"),
-  fs = require("fs"),
-  log = require("fancy-log");
+	through = require("through2"),
+	path = require("path"),
+	fs = require("fs"),
+	log = require("fancy-log");
 
 const sqipModes = [
-  {
-    name: "combo",
-    code: 0
-  },
-  {
-    name: "triangle",
-    code: 1
-  },
-  {
-    name: "rect",
-    code: 2
-  },
-  {
-    name: "ellipse",
-    code: 3
-  },
-  {
-    name: "circle",
-    code: 4
-  },
-  {
-    name: "rotatedrect",
-    code: 5
-  },
-  {
-    name: "beziers",
-    code: 6
-  },
-  {
-    name: "rotatedellipse",
-    code: 7
-  },
-  {
-    name: "polygon",
-    code: 8
-  }
+	{
+		name: "combo",
+		code: 0
+	},
+	{
+		name: "triangle",
+		code: 1
+	},
+	{
+		name: "rect",
+		code: 2
+	},
+	{
+		name: "ellipse",
+		code: 3
+	},
+	{
+		name: "circle",
+		code: 4
+	},
+	{
+		name: "rotatedrect",
+		code: 5
+	},
+	{
+		name: "beziers",
+		code: 6
+	},
+	{
+		name: "rotatedellipse",
+		code: 7
+	},
+	{
+		name: "polygon",
+		code: 8
+	}
 ];
 
 /**
@@ -54,18 +54,18 @@ const sqipModes = [
  * @returns {object} - { final_svg, img_dimensions, svg_base64encoded}
  */
 var makePlaceholder = function(filepath, options) {
-  const resultObject = sqip({
-    filename: filepath,
-    numberOfPrimitives: parseInt(options.primitives),
-    blur: options.blur,
-    mode: options.mode
-  });
-  return resultObject;
+	const resultObject = sqip({
+		filename: filepath,
+		numberOfPrimitives: parseInt(options.primitives),
+		blur: options.blur,
+		mode: options.mode
+	});
+	return resultObject;
 };
 
 var getFileSizeMb = function(filePath) {
-  const stats = fs.statSync(filePath);
-  return stats.size / 1000000.0;
+	const stats = fs.statSync(filePath);
+	return stats.size / 1000000.0;
 };
 
 /**
@@ -73,73 +73,77 @@ var getFileSizeMb = function(filePath) {
  * @param options - primitives, blur and mode
  */
 var gulpSqip = function(options) {
-  if (typeof options == "undefined") {
-    // defaults
-    var options = {
-      blur: 10,
-      mode: 5,
-      primitives: 20,
-      type: "final_svg" // final_svg, img_dimensions, svg_base64encoded
-    };
-    options.includeSource = options.includeSource === false ? false : true;
-  }
+	if (typeof options == "undefined") {
+		// defaults
+		var options = {
+			blur: 10,
+			mode: 5,
+			primitives: 20,
+			type: "final_svg" // final_svg, img_dimensions, svg_base64encoded
+		};
+		options.includeSource = options.includeSource === false ? false : true;
+	}
 
-  return through.obj(function(sourceFile, enc, cb) {
-    const sourceInfo = path.parse(sourceFile.path);
-    if (sourceFile && sourceFile.isBuffer()) {
-      // log which file is being processed
-      let sizeText;
-      try {
-        sizeText = getFileSizeMb(sourceFile.path).toFixed(1) + "mb";
-      } catch (err) {
-        sizeText = "unknown";
-      }
-      log(
-        "Processing '%s' (%s with %s primitives)",
-        sourceInfo.name + sourceInfo.ext,
-        sizeText,
-        options.primitives || "default"
-      );
-      // make the new file
-      const sqipResult = makePlaceholder(sourceFile.path, options)[
-        options.type
-      ];
-      const svgFile = sourceFile.clone();
-      const appendText =
-        options.type !== "final_svg" ? "data:image/svg+xml;base64," : "";
-      svgFile.contents = Buffer.from(`${appendText}${sqipResult}`);
+	return through.obj(function(sourceFile, enc, cb) {
+		const sourceInfo = path.parse(sourceFile.path);
+		if (sourceFile && sourceFile.isBuffer()) {
+			// log which file is being processed
+			let sizeText;
+			try {
+				sizeText = getFileSizeMb(sourceFile.path).toFixed(1) + "mb";
+			} catch (err) {
+				sizeText = "unknown";
+			}
+			log(
+				"Processing '%s' (%s with %s primitives)",
+				sourceInfo.name + sourceInfo.ext,
+				sizeText,
+				options.primitives || "default"
+			);
+			// make the new file
+			const sqipResult = makePlaceholder(sourceFile.path, options)[
+				options.type
+			];
+			const svgFile = sourceFile.clone();
+			const appendText =
+				options.type !== "final_svg"
+					? "data:image/svg+xml;base64,"
+					: "";
+			svgFile.contents = Buffer.from(`${appendText}${sqipResult}`);
 
-      // set new file name
-      const appendName =
-        typeof options.appendName == "string" ? options.appendName : "";
-      const prependName =
-        typeof options.prependName == "string" ? options.prependName : "";
+			// set new file name
+			const appendName =
+				typeof options.appendName == "string" ? options.appendName : "";
+			const prependName =
+				typeof options.prependName == "string"
+					? options.prependName
+					: "";
 
-      svgFile.path = path.join(
-        sourceInfo.dir,
-        prependName +
-          sourceInfo.name +
-          appendName +
-          (options.type !== "final_svg" ? ".txt" : ".svg")
-      );
+			svgFile.path = path.join(
+				sourceInfo.dir,
+				prependName +
+					sourceInfo.name +
+					appendName +
+					(options.type !== "final_svg" ? ".txt" : ".svg")
+			);
 
-      // send file(s) along
-      this.push(svgFile);
-      if (options.includeSource) {
-        this.push(sourceFile);
-      }
-      cb();
-    } else {
-      this.emit(
-        "error",
-        new PluginError(
-          PLUGIN_NAME,
-          "Source must be a file in buffer format: " + sourceFile.path
-        )
-      );
-      cb();
-    }
-  });
+			// send file(s) along
+			this.push(svgFile);
+			if (options.includeSource) {
+				this.push(sourceFile);
+			}
+			cb();
+		} else {
+			this.emit(
+				"error",
+				new PluginError(
+					PLUGIN_NAME,
+					"Source must be a file in buffer format: " + sourceFile.path
+				)
+			);
+			cb();
+		}
+	});
 };
 
 module.exports = gulpSqip;
--- readme.md
+++ readme.md
@@ -4,42 +4,42 @@ Gulpified [sqip](https://www.npmjs.com/package/sqip) that allows full customizat
 
 `sqip` seems to be a work in progress, but it's a cool package that creates SVG impressions of pixel images. Find the right balance between the mode, blur and primitives count, and you get a nice, small LQIP (low quality image placeholder). The default options below make a good placeholder image, but you could make it even smaller by reducing the primitives.
 
-## The (optional) options are:
+## The (optional) options are
 
--   **primitives** - number of primitives a.k.a. the shapes that make up the image (integer, default is 20)
--   **blur** - blur factor (numeric, default is 10)
--   **mode** - mode code (defined below, default is 5)
--   **prependName** - text to prepend to the front of the new SVG filename (string)
--   **appendName** - text to append to the end of the new SVG filename (string)
--   **includeSource** - also pass the source image file through in the stream (boolean, default is false)
+- **primitives** - number of primitives a.k.a. the shapes that make up the image (integer, default is 20)
+- **blur** - blur factor (numeric, default is 10)
+- **mode** - mode code (defined below, default is 5)
+- **prependName** - text to prepend to the front of the new SVG filename (string)
+- **appendName** - text to append to the end of the new SVG filename (string)
+- **includeSource** - also pass the source image file through in the stream (boolean, default is false)
 
-### Mode specifies the base shape for primitives. The mode codes are:
+### Mode specifies the base shape for primitives. The mode codes are
 
--   0 - combo
--   1 - triangle
--   2 - rectangle
--   3 - ellipse
--   4 - circle
--   5 - rotated rectangle
--   6 - beziers
--   7 - rotated ellipse
--   8 - polygon
+- 0 - combo
+- 1 - triangle
+- 2 - rectangle
+- 3 - ellipse
+- 4 - circle
+- 5 - rotated rectangle
+- 6 - beziers
+- 7 - rotated ellipse
+- 8 - polygon
 
-## Example:
+## Example
 
-```
+```js
 const sqippy = require("gulp-sqippy");
 
 gulp.task("make-bg-placeholders", function() {
-    return gulp
-        .src("backgrounds/*.+(png|jpg|jpeg|gif)")
-        .pipe(
-            sqippy({
-                primitives: 12,
-                blur: 6,
-                prependName: "placeholder-",
-            })
-        )
-        .pipe(gulp.dest("backgrounds"));
+  return gulp
+    .src("backgrounds/*.+(png|jpg|jpeg|gif)")
+    .pipe(
+      sqippy({
+        primitives: 12,
+        blur: 6,
+        prependName: "placeholder-"
+      })
+    )
+    .pipe(gulp.dest("backgrounds"));
 });
 ```
